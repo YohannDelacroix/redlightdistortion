5258fd85a588263b056a5348916993682000fd8c
Yohann <yohann.delacroix@hotmail.fr>
2022-12-05 11:55:06 +0100

    Social network icons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,6 +2,8 @@ import "./Footer.css";
 import LogoFb from "../../Assets/Facebook.png";
 import LogoInsta from "../../Assets/Instagram.png";
 import LogoYouTube from "../../Assets/YouTube.png";
+import {SiFacebook, SiInstagram, SiYoutube} from 'react-icons/si'
+import { IconContext } from "react-icons";
 
 function Footer(){
     return (
@@ -14,8 +16,35 @@ function Footer(){
     </div>
 
     <div className="footer-social">
-        <a href="https://www.facebook.com/redlightdistortion/" target="_blank"><img className="footer-social-image" src={LogoFb} alt="f" /></a><a href="https://www.instagram.com/redlightdistortion/" target="_blank"><img className="footer-social-image" src={LogoInsta} alt="f" /></a><a href="https://www.youtube.com/channel/UC1C7_waXIYi6cdy_jCPKoLQ" target="_blank"><img className="footer-social-image" src={LogoYouTube} alt="f" /></a>
+      {
+        /* 
+                <a href="https://www.facebook.com/redlightdistortion/" target="_blank"><img className="footer-social-image" src={LogoFb} alt="f" /></a><a href="https://www.instagram.com/redlightdistortion/" target="_blank"><img className="footer-social-image" src={LogoInsta} alt="f" /></a><a href="https://www.youtube.com/channel/UC1C7_waXIYi6cdy_jCPKoLQ" target="_blank"><img className="footer-social-image" src={LogoYouTube} alt="f" /></a>
+
+        */
+      }
+        <a href="https://www.facebook.com/redlightdistortion/" target="_blank">
+          <IconContext.Provider value={{className: 'footer-react-icons', size:'25px'}}>
+              <div>
+              <SiFacebook />
+              </div>
+              
+          </IconContext.Provider>
+        </a>
+        
+        <a href="https://www.instagram.com/redlightdistortion/" target="_blank">
+        <IconContext.Provider value={{className: 'footer-react-icons', size:'25px'}}>
+                        <div><SiInstagram /></div>
+        </IconContext.Provider>
+      
+        </a>
+        
+        <a href="https://www.youtube.com/channel/UC1C7_waXIYi6cdy_jCPKoLQ" target="_blank">
+        <IconContext.Provider value={{className: 'footer-react-icons', size:'25px'}}>
+                        <div><SiYoutube /></div>
+          </IconContext.Provider>
+        </a>
     </div>
+
     </div>)
 }
 
