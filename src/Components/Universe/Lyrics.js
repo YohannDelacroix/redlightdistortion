65756ab2fa54d289bec8cc2808dda604a671b148
Yohann <yohann.delacroix@hotmail.fr>
2022-12-14 16:15:56 +0100

    universelyricsfrontend

--- src/Components/Universe/Lyrics.js
+++ src/Components/Universe/Lyrics.js
@@ -34,15 +34,61 @@ const Lyrics = () => {
     },[])
 
     //Display the lyrics object in the console (Debug)
+    /*
     useEffect( () => {
-        console.log(lyrics)
-    }, lyrics)
+        console.log("LYRICS OBJECT :", lyrics)
+    }, [lyrics])
+    */
 
-
+    
     //Change the song displayed on screen
     const handleDisplayLyrics = (song) => {
         setLyricsSection(true)
         setSong(song)
+        displayLyricsSection(true, song)
+    }
+
+    //Manage the Lyrics section on screen and the effects
+    const displayLyricsSection = (active, currentSong=null) => {
+
+        console.log("UPDATED : ", document.getElementById("universe-song"))
+
+        if(active){
+            if(document.getElementById("universe-song") == null){
+                console.log("NULL")
+            }
+            else
+            {
+                document.getElementById("universe-song").classList.remove("us-deleteitem");
+                document.getElementById("universe-song").classList.add("us-newitem");
+                document.getElementById("universe-song-button-close").classList.remove("disappear-animation");
+                /*if(document.getElementById("universe-song-button-close").classList.contains("disappear-animation")){
+                    //document.getElementById("universe-song-button-close").classList.remove("disappear-animation");
+                }*/
+                
+                const heightValue = currentSong.lyrics_en.length * 20;
+                console.log(heightValue, "px");
+
+                /*const setHeight = () => {
+                    document.getElementById("universe-song").style.maxHeight = `${heightValue+200}px`;
+                }
+            
+                window.requestAnimationFrame(setHeight)*/
+            }
+        }
+        else{
+            //setLyricsSection(false)
+            document.getElementById("universe-song").classList.remove("us-newitem");
+            document.getElementById("universe-song").classList.add("us-deleteitem");
+            document.getElementById("universe-song-button-close").classList.add("disappear-animation");
+
+            /*const setHeight = () => {
+                document.getElementById("universe-song").style.maxHeight = `0px`;
+            }*/
+
+        }
+        
+        
     }
 
     return (
@@ -51,13 +97,13 @@ const Lyrics = () => {
         {error && <div>Problem fetching datas with server</div>}   
 
         <div className="lyrics-container">
-        {
-        //Here is the part displaying the lyrics on screen
-        lyricsSection && <div className="universe-song">
+        
+            {lyricsSection && <div id="universe-song" className="universe-song us-newitem">
                 <div className="universe-song-top-section">
                 <h5 className="universe-song-top-section-title">{song.title}</h5>
-                <button className="universe-song-button-close"
-                        onClick={ () => setLyricsSection(false)}>X</button>
+                <button id="universe-song-button-close"
+                        className="universe-song-button-close"
+                        onClick={ () => displayLyricsSection(false)}>X</button>
                 <div className='universe-song-top-section-line'></div>
                 </div>
                 
@@ -69,13 +115,15 @@ const Lyrics = () => {
                     <h5>Lyrics</h5>
                     {song.lyrics_en.map( (sentence, index) => (<div key={index} className="universe-song-lyrics-sentence">{sentence}</div>))}
                 </div>
-        </div>}
+            </div>}
+        
+        
 
         {
         //List of songs that the user can select to see the lyrics
         lyrics && <ul className="universe-songs-list">
             {lyrics.map( (song, index) => {
-                console.log(song)
+                //console.log(song)
                 return(
                     <li key={`${index}+${song.title}`} className="universe-songs-list-item">
                         <button onClick={() => handleDisplayLyrics(song)}
