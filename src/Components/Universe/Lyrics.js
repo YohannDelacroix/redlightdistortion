8e9493d6f6776dfe44a93c07c75de0a85fffc47b
Yohann <yohann.delacroix@hotmail.fr>
2022-12-23 17:36:04 +0100

    lyrics frontend finished

--- src/Components/Universe/Lyrics.js
+++ src/Components/Universe/Lyrics.js
@@ -11,7 +11,8 @@ const Lyrics = () => {
     const [song, setSong] = useState(emptySong);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-    //const [lyricsSection, setLyricsSection] = useState(false)
+    const [lyricsSection, setLyricsSection] = useState(false)
+    const [stopResizing, setStopResizing] = useState(new AbortController());
 
 
     //Load the lyrics from a JSON file
@@ -33,117 +34,119 @@ const Lyrics = () => {
      }
 
     getLyrics();
-    document.getElementById("universe-song").style.maxHeight = `0px`;
-    document.getElementById("universe-song").style.minHeight = `0px`;
-    },[])
-
-
-    //Display the lyrics object in the console (Debug)
-    /*
-    useEffect( () => {
-        console.log("LYRICS OBJECT :", lyrics)
-    }, [lyrics])*/
-    
+        if(!setLyricsSection){
+            document.getElementById("universe-song").style.maxHeight = `0px`;
+            document.getElementById("universe-song").style.minHeight = `0px`;
+        }
+    },[setLyricsSection])
 
     
     //Change the song displayed on screen
     const handleDisplayLyrics = (song) => {
-        //setLyricsSection(true)
+        console.log("okdakk")
+        setLyricsSection(true)
         setSong(emptySong) //Allow the user to go back to the song he closed
         setSong(song)
-        //displayLyricsSection(true, song)
+        displayLyricsSection(true, song)
     }
 
-    //When the song changes, it change the size of the lyrics section on screen with transitions
-    useEffect(() => {
-        console.log("WORK ?")
-        if(song !== emptySong){
-            displayLyricsSection(true, song)
-        }
-    },[song])
 
     //Manage the Lyrics section on screen and the effects
     const displayLyricsSection = (active, currentSong=null) => {
-
-        console.log("UPDATED : ", document.getElementById("universe-song"))
         const elemUniverseSong = document.getElementById("universe-song");
-        const resizeLyrics = new AbortController();
+        
+
+        /* 
+        
+         Making the lyrics expanding at the perfect height 
+  
+        */
+
+            //1 - Request the browser to play an animation on screen
+            const triggerHeightAnimation = () => {
+                const callSetHeight = () => {
+                    setHeight(calcHeight());
+                }
+                window.requestAnimationFrame(callSetHeight)
+            }
+
+            //2 - Calcul the height of the universe-song div
+            const calcHeight = () => {
+                let descriptionElement = document.getElementById("universe-song-description")
+                let descriptionLength = 0;
+                for(let child of descriptionElement.children){
+                    descriptionLength += child.offsetHeight;
+                }
+                const heightValue = currentSong.lyrics_en.length * 20 + descriptionLength + 120; 
+                return heightValue;
+            }
+
+            //3 - Set the min and max height before the transition animation
+            const setHeight = (heightValue) => {
+                elemUniverseSong.style.maxHeight = `${heightValue}px`;
+                elemUniverseSong.style.minHeight = `${heightValue}px`;
+            }
+
+
+        /*
+            Resizing event
+        */
+
+            //Prevent to execute the function only one time after the end of resizing
+            const afterResize = () => {
+                triggerHeightAnimation();
+            }
+
+            let timeOutFunctionID;
+            //Each time the event window.resize is triggered
+            const resizeWindow = () => {
+                clearTimeout(timeOutFunctionID);
+                timeOutFunctionID = setTimeout(afterResize, 500);
+            }
 
         
-        const afterResize = () => {
-            console.log("After resize")
-            triggerAnimation();
-        }
 
-        let timeOutFunctionID;
-        const resizeWindow = () => {
-            console.log("Resizing window");
-            clearTimeout(timeOutFunctionID);
-            timeOutFunctionID = setTimeout(afterResize, 500);
-        }
+        /* 
+            Display the lyrics section or close 
+        */
 
-        const setHeight = (heightValue) => {
-            console.log("SET SET", heightValue)
-            elemUniverseSong.style.maxHeight = `${heightValue}px`;
-            elemUniverseSong.style.minHeight = `${heightValue}px`;
-        }
-
-        //Calcul the height of the universe-song div
-        const calcHeight = () => {
-            let descriptionElement = document.getElementById("universe-song-description")
-            let descriptionLength = 0;
-            for(let child of descriptionElement.children){
-                descriptionLength += child.offsetHeight;
+            if(active){  //When opening a new lyric
+                if(document.getElementById("universe-song") == null){
+                    console.log("Error : universe-song undeclared = NULL")
+                }
+                else
+                {
+                    elemUniverseSong.classList.remove("us-deleteitem");
+                    elemUniverseSong.classList.add("us-newitem");
+                    document.getElementById("universe-song-button-close").classList.remove("disappear-animation");
+                    
+                    triggerHeightAnimation(); //Height transition animation
+                    
+                    window.addEventListener("resize", resizeWindow, stopResizing);
+                }
             }
-            console.log("desc l : ", descriptionLength)
-            console.log((currentSong.lyrics_en.length * 20), "lyrics")
-            const heightValue = currentSong.lyrics_en.length * 20 + descriptionLength + 120;
-            console.log("heightValue = ", heightValue, "px");
-            return heightValue;
-        }
+            else{ //When Closing the lyrics section
+                setLyricsSection(false)
+                elemUniverseSong.classList.remove("us-newitem");
+                elemUniverseSong.classList.add("us-deleteitem");
+                document.getElementById("universe-song-button-close").classList.add("disappear-animation");
 
-        const triggerAnimation = () => {
-            const callSetHeight = () => {
-                setHeight(calcHeight());
-            }
-            window.requestAnimationFrame(callSetHeight)
-        }
+                //Set the height to 0 and trigger an animation
+                const setHeight = () => {
+                    elemUniverseSong.style.maxHeight = `0px`;
+                    elemUniverseSong.style.minHeight = `0px`;
+                }
+                window.requestAnimationFrame(setHeight);
 
-        if(active){
-            if(document.getElementById("universe-song") == null){
-                console.log("universe-song undeclared = NULL")
-            }
-            else
-            {
-                elemUniverseSong.classList.remove("us-deleteitem");
-                elemUniverseSong.classList.add("us-newitem");
-                document.getElementById("universe-song-button-close").classList.remove("disappear-animation");
-                /*if(document.getElementById("universe-song-button-close").classList.contains("disappear-animation")){
-                    //document.getElementById("universe-song-button-close").classList.remove("disappear-animation");
-                }*/
-                
-            
-                
-                triggerAnimation();
-                
-                //window.addEventListener("resize", resizeWindow, resizeLyrics);
-            }
-        }
-        else{
-            //setLyricsSection(false)
-            elemUniverseSong.classList.remove("us-newitem");
-            elemUniverseSong.classList.add("us-deleteitem");
-            document.getElementById("universe-song-button-close").classList.add("disappear-animation");
+                //Prevent the height to increase when the user resize the screen when there is no song on screen
+                stopResizing.abort();
+                setStopResizing(new AbortController());
 
-            const setHeight = () => {
-                elemUniverseSong.style.maxHeight = `0px`;
-                elemUniverseSong.style.minHeight = `0px`;
+                //Remove the class deleteitem to prevent the song list moving when screen is resized, 500 is the duration of the animation
+                setTimeout( () => {
+                    elemUniverseSong.classList.remove("us-deleteitem");
+                }, 500);
             }
-            window.requestAnimationFrame(setHeight);
-
-            //resizeLyrics.abort();
-            //window.removeEventListener("resize", resizeWindow);
-        }
     }
 
     return (
