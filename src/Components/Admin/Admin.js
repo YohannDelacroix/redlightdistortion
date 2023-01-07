cc39c7faf5aa3b2caf263b58c3d60f62ea9e254e
Yohann <yohann.delacroix@hotmail.fr>
2023-01-07 13:26:19 +0100

    Frontend Admin Menu

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -10,24 +10,67 @@ import Subscribers from './Subscribers';
 import Tour from './Tour'
 import News from './News'
 import Lyrics from "./Lyrics";
-
+import TitleComponent from '../TitleComponent/TitleComponent'
 
 
 export default function Admin(){
+    const DISPLAY_NONE = 0;
+    const DISPLAY_TOUR = 1;
+    const DISPLAY_LYRICS = 2;
+
+    const [display, setDisplay] = useState(DISPLAY_NONE);
+
+    //Apply a class on the button selected
+    const buttonFocus = (id) => {
+        let buttons = document.querySelectorAll('.admin-menu-choice > button');
+        for(let button of buttons){
+            if(button.id === id.toString()){
+                console.log(button);
+                button.classList.add("admin-menu-choice-button-focus");
+            }
+            else{
+                console.log(button);
+                button.classList.remove("admin-menu-choice-button-focus");
+            }
+        }
+    }
+
+    const handleDisplayTour = () => {
+        setDisplay(DISPLAY_TOUR);
+        buttonFocus(DISPLAY_TOUR);
+    }
+
+    const handleDisplayLyrics = () => {
+        setDisplay(DISPLAY_LYRICS);
+        buttonFocus(DISPLAY_LYRICS);
+    }
+
+    const handleDisplayNone = () => {
+        setDisplay(DISPLAY_NONE);
+        buttonFocus(DISPLAY_NONE);
+    }
+
+
 
     return (<div>
         <Header />
         <div className="admin-container">
-            {
-                //<Subscribers />
-            }           
-            <Tour />
-            {
-                /*
-                    <News />
-                */
-            }
-            <Lyrics />
+        <menu className="admin-menu-choices">
+            <li className="admin-menu-choice">
+                <button id={DISPLAY_TOUR} onClick={handleDisplayTour}>
+                    <TitleComponent titleContent="TOUR" />
+                </button>
+            </li>
+            <li className="admin-menu-choice">
+                <button id={DISPLAY_LYRICS} onClick={handleDisplayLyrics}>
+                    <TitleComponent titleContent="LYRICS" />
+                </button>
+            </li>
+        </menu>
+
+
+        {display === DISPLAY_TOUR && <Tour />}
+        {display === DISPLAY_LYRICS && <Lyrics />}
             
         </div> 
         <Footer />
