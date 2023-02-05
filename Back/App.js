cd4dd3705dcc186e512dcdedb6a515065c962bae
Yohann <yohann.delacroix@hotmail.fr>
2023-02-05 13:55:18 +0100

    Backend, cors, credentials, allowedOrigins

--- Back/App.js
+++ Back/App.js
@@ -4,22 +4,18 @@ const fs = require('fs');
 const { ppid } = require('process');
 const functions = require('./Functions')
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const credentials = require('./middleware/credentials')
+const corsOptions = require('./config/corsOptions');
 
 const path = require('path');
 require('dotenv').config( {
   path: path.join(__dirname, "views", ".env")
 });
 
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin',
-    '*');
-    res.setHeader('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-  
+app.use(credentials);
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 
